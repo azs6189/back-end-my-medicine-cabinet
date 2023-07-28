ed421180228ebe9a95a86336c8316bc6268a99cb
Allie Soliz <solizale@gmail.com>
2023-07-28 19:11:19 -0400

    adds template loader to view.medications function | adds class Medications

--- medicine_cabinet/medications/models.py
+++ medicine_cabinet/medications/models.py
@@ -1,3 +1,12 @@
+# A Django model is a table in your database
+
 from django.db import models
 
 # Create your models here.
+class Medication(models.Model):
+    brand_name = models.CharField(max_length=255, blank=True, null=True)
+    medication_description = models.CharField(max_length=255, blank=True, null=True)
+    pap_name = models.CharField(max_length=255, blank=True, null=True)
+    pap_info_link = models.URLField(max_length=200, blank=True, null=True)
+    pap_eligibility_link = models.URLField(max_length=200, blank=True, null=True)
+    pap_application_link = models.URLField(max_length=200, blank=True, null=True)
