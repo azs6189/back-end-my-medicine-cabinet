8d695d8a65e87c939c70edc37ce605de7ce2da83
Allie Soliz <solizale@gmail.com>
2023-07-31 00:13:10 -0400

    converts function-based views to class-based views

--- medicine_cabinet/medications/views.py
+++ medicine_cabinet/medications/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParse
+from medications.models import Medication, MedicationSerializer
+# from django.template import loader
 
 # Create your views here.
+# def medications(request):
+#     template = loader.get_template('myfirst.html')
+#     return HttpResponse(template.render())
 
-def medications(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+class MedicationList(APIView):
+    def get(self, request):
+        medications = Medication.objects.all()
+        serializer = MedicationSerializer(medications, many=True)
+        return JsonResponse(serializer.data, safe=False)
