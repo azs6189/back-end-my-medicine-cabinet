db5ffd31065d1d818be5b2f824f15263d8103642
Allie Soliz <solizale@gmail.com>
2023-07-31 01:13:08 -0400

    adds HTTP method: POST to /medicationlist/ endpoint

--- medicine_cabinet/medications/views.py
+++ medicine_cabinet/medications/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
-from rest_framework.parsers import JSONParse
+from rest_framework.parsers import JSONParser
 from medications.models import Medication, MedicationSerializer
 # from django.template import loader
 
@@ -11,7 +11,19 @@ from medications.models import Medication, MedicationSerializer
 #     return HttpResponse(template.render())
 
 class MedicationList(APIView):
+    """
+    List all medications, or create a new medication
+    """
     def get(self, request):
         medications = Medication.objects.all()
         serializer = MedicationSerializer(medications, many=True)
         return JsonResponse(serializer.data, safe=False)
+    
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = MedicationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
+    
