23ba6952019c4622619e9bac7bdc418401243f14
Allie Soliz <solizale@gmail.com>
2023-08-02 10:06:20 -0400

    updates app urlpatterns to include path to medications/id

--- medicine_cabinet/medications/urls.py
+++ medicine_cabinet/medications/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('medications/', views.medications, name='medications')
+    path('medications/', views.MedicationList.as_view()),
+    path('medications/<int:pk>/', views.MedicationDetail.as_view())
 ]
