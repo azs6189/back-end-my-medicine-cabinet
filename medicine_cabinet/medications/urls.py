2d431fcba516d592f74be8e7d40bdc8d39d57a17
Allie Soliz <solizale@gmail.com>
2023-08-03 12:12:56 -0400

    adds name to urlpattern medications/id

--- medicine_cabinet/medications/urls.py
+++ medicine_cabinet/medications/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('medications/', views.MedicationList.as_view()),
-    path('medications/<int:pk>/', views.MedicationDetail.as_view())
+    path('medications/<int:pk>/', views.MedicationDetail.as_view(), name='medication-detail')
 ]
