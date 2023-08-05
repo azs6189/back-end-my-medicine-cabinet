2e40be84015b46ba43ab92e93e6baa07c6d99df8
Allie Soliz <solizale@gmail.com>
2023-08-04 20:28:56 -0400

    secret_key moved to env

--- medicine_cabinet/settings.py
+++ medicine_cabinet/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ipd&7!p1(m2m5(#lk&$ryll$je$i=!kl7&d*7e%lgxco+i*nl9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
