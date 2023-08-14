e3eac8b68df7efbf17af1487b1f232602770d80e
Allie Soliz <solizale@gmail.com>
2023-08-13 23:00:25 -0400

    adds cors settings

--- medicine_cabinet/settings.py
+++ medicine_cabinet/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'medications'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # "https://example.com",
+    # "https://frontend.example.com",
+    "http://localhost:3000",  # Example for local development
 ]
 
 ROOT_URLCONF = 'medicine_cabinet.urls'
